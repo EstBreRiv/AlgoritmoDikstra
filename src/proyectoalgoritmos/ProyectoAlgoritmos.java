01cc9cd8087d08c6e4297877639bb5b8c4367577
EstBreRiv <estebanjosuebrenes29@gmail.com>
2024-10-05 15:49:02 -0600

    Algoritmo de dikstra

---
+++ src/proyectoalgoritmos/ProyectoAlgoritmos.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoalgoritmos;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Esteban Brenes Rivera
+ * @version 0.9 Dijkstra y funcionalidaes Completados
+ */
+public class ProyectoAlgoritmos {
+
+    public static void main(String[] args) {
+//        ListaEnlazadaVertices listaVertices = new ListaEnlazadaVertices();
+//        ListaEnlazadaAristas lista1 = new ListaEnlazadaAristas();
+//        ListaEnlazadaAristas lista2 = new ListaEnlazadaAristas();
+//        ListaEnlazadaAristas lista3 = new ListaEnlazadaAristas();
+//        ListaEnlazadaAristas lista4 = new ListaEnlazadaAristas();
+//        ListaEnlazadaAristas lista5 = new ListaEnlazadaAristas();
+//        ListaEnlazadaAristas lista6 = new ListaEnlazadaAristas();
+//        ListaEnlazadaAristas lista7 = new ListaEnlazadaAristas();
+//        ListaEnlazadaAristas lista8 = new ListaEnlazadaAristas();
+//        ListaEnlazadaAristas lista9 = new ListaEnlazadaAristas();
+//        ListaEnlazadaAristas lista10 = new ListaEnlazadaAristas();
+//
+//        Arista arista = new Arista("Ciudad Quesada", "Zarcero", 28, 46);
+//        Arista arista2 = new Arista("Zarcero", "Naranjo", 19, 31);
+//        Arista arista3 = new Arista("Naranjo", "Sarchí", 6, 11);
+//        Arista arista4 = new Arista("Sarchí", "Grecia", 15, 27);
+//        Arista arista5 = new Arista("Grecia", "Tacares", 12, 26);
+//        Arista arista6 = new Arista("Tacares", "Alajuela", 11, 27);
+//        Arista arista7 = new Arista("San Ramón", "Zarcero", 22, 37);
+//        Arista arista8 = new Arista("San Ramón", "Palmares", 7, 12);
+//        Arista arista9 = new Arista("Palmares", "Alajuela", 39, 120);
+//        Arista arista10 = new Arista("Alajuela", "Palmares", 39, 40);
+//        Arista arista11 = new Arista("Grecia", "Palmares", 26, 30);
+//        Arista arista12 = new Arista("Palmares", "Naranjo", 12, 16);
+//        Arista arista13 = new Arista("Sarchí", "Naranjo", 6, 22);
+//        Arista arista14 = new Arista("Grecia", "Sarchí", 15, 45);
+//        Arista arista15 = new Arista("Tacares", "Grecia", 12, 26);
+//        Arista arista16 = new Arista("Alajuela", "Tacares", 11, 27);
+//        Arista arista17 = new Arista("Grecia", "San Pedro Poás", 14, 30);
+//        Arista arista18 = new Arista("San Pedro Poás", "Alajuela", 11, 23);
+//        Arista arista19 = new Arista("Palmares", "San Ramón", 7, 8);
+//
+//        lista1.insertarFinal(arista);
+//
+//        lista2.insertarFinal(arista2);
+//
+//        lista3.insertarFinal(arista3);
+//
+//        lista4.insertarFinal(arista4);
+//        lista4.insertarFinal(arista13);
+//
+//        lista5.insertarFinal(arista5);
+//        lista5.insertarFinal(arista11);
+//        lista5.insertarFinal(arista14);
+//        lista5.insertarFinal(arista17);
+//
+//        lista6.insertarFinal(arista18);
+//
+//        lista7.insertarFinal(arista10);
+//        lista7.insertarFinal(arista16);
+//
+//        lista8.insertarFinal(arista6);
+//        lista8.insertarFinal(arista15);
+//
+//        lista9.insertarFinal(arista19);
+//        lista9.insertarFinal(arista12);
+//        lista9.insertarFinal(arista9);
+//
+//        lista10.insertarFinal(arista7);
+//        lista10.insertarFinal(arista8);
+//
+//        listaVertices.insertarFinal(new Vertice("Ciudad Quesada", lista1));
+//        listaVertices.insertarFinal(new Vertice("Zarcero", lista2));
+//        listaVertices.insertarFinal(new Vertice("Naranjo", lista3));
+//        listaVertices.insertarFinal(new Vertice("Sarchí", lista4));
+//        listaVertices.insertarFinal(new Vertice("Grecia", lista5));
+//        listaVertices.insertarFinal(new Vertice("San Pedro Poás", lista6));
+//        listaVertices.insertarFinal(new Vertice("Alajuela", lista7));
+//        listaVertices.insertarFinal(new Vertice("Tacares", lista8));
+//        listaVertices.insertarFinal(new Vertice("Palmares", lista9));
+//        listaVertices.insertarFinal(new Vertice("San Ramón", lista10));
+//
+//        System.out.println("Distancia mas corta:");
+////        System.out.println(listaVertices.calcularTiempo("Ciudad Quesada"));
+////        System.out.println(listaVertices.calcularDistancia("Ciudad Quesada"));
+//
+//        JOptionPane.showInputDialog(null, listaVertices.dijskstra("San Ramón"));
+////        JOptionPane.showConfirmDialog(null, listaVertices.disjkstraDistancia("Zarcero", "Tacares"));
+//
+////        System.out.println(listaVertices.imprimir());
+        Menu menu = new Menu();
+        menu.menu();
+    }
+
+}
